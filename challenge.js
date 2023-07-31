95612d20aa02978ce218e2780a141660d633f316
Huu Duc Nguyen <ducnrg@gmail.com>
2023-07-31 00:03:01 -0400

    add function step1()

--- challenge.js
+++ challenge.js
@@ -8,22 +8,56 @@ if (args.length !== 1) {
 }
 const filename = args[0];
 readFileAndCleanData(filename);
-
+console.log(step1(filename));
 
 // read the file and remove all non-alphanumeric characters
 function readFileAndCleanData(filename) {
-    const data =fs.readFileSync(filename, "utf8", function (err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        return data;
+  const data = fs.readFileSync(filename, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    return data;
+  });
+  const cleanData = data.replace(/[^a-zA-Z0-9,]/g, "");
+  fs.writeFileSync(filename, cleanData, function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+}
+
+// step1 -> read file and count string
+function step1(filename) {
+  const data = fs.readFileSync(filename, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    return data;
+  });
+    
+  const splitedData = data.split(",");
+  const countMap = {};
+  for (s of splitedData) {
+    if (countMap[s]) countMap[s] += 1;
+    else countMap[s] = 1;
+  }
+
+  const sortedCountMap = {};
+  Object.keys(countMap)
+    .sort(function (a, b) {
+      // if both are numbers
+      if (isNaN(a) && isNaN(b)) return a.localeCompare(b);
+
+      if (isNaN(a)) return 1;
+      if (isNaN(b)) return -1;
+      // if both are not numbers
+      return parseInt(a) - parseInt(b);
+    })
+    .forEach(function (key) {
+      sortedCountMap[key] = countMap[key];
     });
-    const cleanData = data.replace(/[^a-zA-Z0-9,]/g, "");
-    fs.writeFileSync(filename, cleanData, function (err) { 
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-    });
+  return sortedCountMap;
 }
